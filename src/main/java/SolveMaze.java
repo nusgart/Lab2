1569152c08c63f1452c2754f8d21c357688fffd3
Nicholas Nusgart <nicknusgart@gmail.com>
2018-01-31 12:43:53 -0600

    Current progress on lab 2

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -1,5 +1,8 @@
 import edu.illinois.cs.cs125.lib.mazemaker.Maze;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Solve a randomly-generated maze.
  *
@@ -28,14 +31,57 @@ public class SolveMaze {
          */
         maze.startAtZero();
         maze.endAtTopRight();
-
+        JFrame j = new JFrame("Test");
+        j.setSize(1024, 768);
+        JTextArea area = new JTextArea(20, 20);
+        area.setFont(new Font("monospaced", Font.PLAIN, 20));
+        j.add(area);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setVisible(true);
         /*
          * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
          * Feel free to adjust this number if you experiment with other mazes.
          */
+        boolean hadWall = true;
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+//            while(!maze.isFinished() ){
+            // check for wall
+            maze.turnRight();
+            boolean haveWall = !maze.canMove();
+            maze.turnLeft();
+            //
+            maze.turnLeft();
+            boolean haveLeftWall = !maze.canMove();
+            maze.turnRight();
+
+            if(!maze.canMove()){
+                if(haveWall) maze.turnLeft();
+                else maze.turnRight();
+            }
+
+            if(haveWall && maze.canMove()){
+                maze.move();
+                continue;
+            }
+
+            // check to see if wall ended
+            if (!haveWall && hadWall) {
+                maze.turnRight();
+            }
+            hadWall = haveWall;
+            maze.move();
+            area.setText(maze.toString());
+            j.repaint();
+            try {
+                Thread.sleep(10);
+            }catch(Throwable t){}
+            System.out.println(maze.toString());
+//            }
+
         }
+
+        System.out.println(maze.toString());
 
         if (maze.isFinished()) {
             System.out.println("You solved the maze!");
@@ -44,3 +90,13 @@ public class SolveMaze {
         }
     }
 }
+/*
+if(!maze.canMove()){
+                    maze.turnRight();
+                    if(!maze.canMove()){
+                        maze.turnLeft();
+                        maze.turnLeft();
+                        maze.turnLeft();
+                    }
+                }
+ */
